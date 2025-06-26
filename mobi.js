36d518e8a0c96a4e0951e796af35196c18cbe7f0
ISHAN PAL <ishu4.op@gmail.com>
2025-06-26 23:13:32 +0530

    Update mobi.js

--- mobi.js
+++ mobi.js
@@ -1,9 +1,21 @@
-const sidebar = document.querySelector('.sidebar');
-const btn     = document.querySelector('.toggle-btn');
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
 
-btn.addEventListener('click', () => {
-  // for desktop collapse/expand
-  sidebar.classList.toggle('collapsed');
-  // for mobile slide-in/out
-  sidebar.classList.toggle('open');
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
 });
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
+
